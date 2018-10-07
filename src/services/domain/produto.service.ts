154ced04f87d6f17132fa16d648dfda7ff7cfa7c
FERNANDO STAROSKI <fernnadostaroski@gmail.com>
2018-10-07 11:57:22 -0300

    carregar dados dos produtos

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -6,13 +6,25 @@ import { API_CONFIG } from "../../config/api.config";
 
 @Injectable()
 export class ProdutoService {
+    
     constructor(public http: HttpClient) {
     }
+
     findByCategoria(categoria_id : string): Observable<ProdutoDTO[]> {
         return this.http.get<ProdutoDTO[]>(`${API_CONFIG.baseUrl}/produtos?categorias=${categoria_id}`);
     }
+
     getSmallImageFromBucket(id: string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`;
+        return this.http.get(url,{responseType: 'blob'});
+    }
+
+    findById(produto_id: string): Observable<ProdutoDTO> {
+        return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
+    }
+
+    getImageFromBucket(id: string): Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`;
         return this.http.get(url,{responseType: 'blob'});
     }
 }
