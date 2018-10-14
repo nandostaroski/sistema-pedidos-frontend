b0944b57267c05280dafe7bf974ba3af80f14353
FERNANDO STAROSKI <fernnadostaroski@gmail.com>
2018-10-14 15:14:40 -0300

    adicionando infinity scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -11,7 +11,8 @@ import { API_CONFIG } from '../../config/api.config';
 })
 export class ProdutosPage {
 
-  items: ProdutoDTO[];
+  items: ProdutoDTO[] =[];
+  page: number = 0;
 
   constructor(
     public navCtrl: NavController, 
@@ -25,21 +26,23 @@ export class ProdutosPage {
   }
 
   loadData(){
-    let categoria_id = this.navParams.get('categoria_id');
-    let loader = this.presentLoading();
+    const categoria_id = this.navParams.get('categoria_id');
+    const loader = this.presentLoading();
     this.produtoService
-    .findByCategoria(categoria_id)
+    .findByCategoria(categoria_id, this.page, 10)
     .subscribe(response =>{
-      this.items = response['content'];
-      this.loadImageUrls();
+      const start = this.items.length;
+      this.items = this.items.concat(response['content']);
+      const end = this.items.length;
+      this.loadImageUrls(start, end);
     },error=>{},
     ()=>{
       loader.dismiss();
     });
   }
 
-  loadImageUrls() {
-    for (var i=0; i<this.items.length;i++) {
+  loadImageUrls(start:number, end: number) {
+    for (var i=start; i<end; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
@@ -62,9 +65,20 @@ export class ProdutosPage {
   }
 
   doRefresh(refresher) {
+    this.page = 0;
+    this.items = [];
     this.loadData();
     setTimeout(() => {
       refresher.complete();
+    }, 1000);
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+      
+      infiniteScroll.complete();
     }, 1000);
   }
 }
